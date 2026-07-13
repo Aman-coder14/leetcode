83bd5caa7b8c030898654f3dcf59b8df3bb25ebc
Aman-coder14 <Amansingh81028@gmail.com>
2026-07-13 15:08:27 +0530

    Time: 0 ms (100%), Space: 11.7 MB (27.81%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-       //int c=0;
+        int n=digits.size();
         for(int i=n-1;i>=0;i--){
             if(digits[i]<9){
                 digits[i]++;
