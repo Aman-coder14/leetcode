cbb9c038e5b496bfca32244b8ff5ff424c729666
Aman-coder14 <Amansingh81028@gmail.com>
2026-07-19 12:24:43 +0530

    Time: 76 ms (20.13%), Space: 220.4 MB (29.99%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,38 +1,19 @@
 class MyHashMap {
 public:
-    vector<vector<pair<int, int>>> table;
-    int size = 1000;
-    MyHashMap() { table.resize(size); }
-
+vector<int>mp;
+    MyHashMap() {
+        mp.resize(1000001,-1);
+    }
+    
     void put(int key, int value) {
-        int index = key % size;
-        for (auto &p : table[index]) {
-            if (p.first == key) {
-                p.second = value;
-                return;
-            }
-        }
-        table[index].push_back({key, value});
+        mp[key]=value;
     }
-
+    
     int get(int key) {
-        int index = key % size;
-        for (auto &p : table[index]) {
-            if (p.first == key)
-                return p.second;
-
-        }
-        return -1;
+        return mp[key];
     }
-
+    
     void remove(int key) {
-        int index = key % size;
-
-        for (int i = 0; i < table[index].size(); i++) {
-            if (table[index][i].first == key) {
-                table[index].erase(table[index].begin() + i);
-                return;
-            }
-        }
+        mp[key]=-1;
     }
 };
