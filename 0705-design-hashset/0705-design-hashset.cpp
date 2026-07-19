a0f1bc562149f4070c0b51e073ce75decb3efd36
Aman-coder14 <Amansingh81028@gmail.com>
2026-07-19 12:28:53 +0530

    Time: 8 ms (84.66%), Space: 49.5 MB (52.82%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,33 +1,11 @@
 class MyHashSet {
 public:
-    vector<vector<int>> table;
-    int size = 1000;
-    MyHashSet() { table.resize(size); }
-    void add(int key) {
-        int index = key % size;
-        for (int x : table[index]) {
-            if (x == key) {
-                return;
-            }
-        }
-          table[index].push_back(key);
-    }
-    void remove(int key) {
-        int index = key % size;
-        for (int i = 0; i < table[index].size(); i++) {
-            if (table[index][i] == key) {
-                table[index].erase(table[index].begin() + i);
-                return;
-            }
-        }
-    }
-    bool contains(int key) {
-        int index = key % size;
-        for (int x : table[index]) {
-            if (x == key) {
-                return true;
-            }
-        }
-        return false;
-    }
+    vector<bool> mp;
+    MyHashSet() { mp.resize(1000001, false); }
+
+    void add(int key) { mp[key] = true; }
+
+    void remove(int key) { mp[key] = false; }
+
+    bool contains(int key) { return mp[key]; }
 };
