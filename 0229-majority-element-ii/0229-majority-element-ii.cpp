25dde8b7c10b76fe6cbaee61d4a593b6b77688b4
Aman-coder14 <Amansingh81028@gmail.com>
2026-07-25 14:34:48 +0530

    Time: 0 ms (100%), Space: 23.6 MB (67.19%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int ele1=0,ele2=0;
-        int count1=0,count2=0;
-        for(int num : nums){
-            if(num==ele1){
-                count1++;
-            }else if(num==ele2){
-                count2++;
-            }else if(count1==0){
-                ele1=num;
-                count1++;
-            }else if(count2==0){
-                ele2=num;
-                count2++;
-            }else{
-                count1--;
-                count2--;
-            }
+      int n=nums.size();
+      int count1=0,count2=0;
+      int ele1=0,ele2=0;
+      for(int i=0;i<n;i++){
+        int num=nums[i];
+        if(num==ele1){
+            count1++;
+        }else if(num==ele2){
+            count2++;
+        }else if(count1==0){
+            ele1=num;
+            count1=1;
         }
-        count1=0;
-        count2=0;
-        for(int num:nums){
-            if(num==ele1){
-                count1++;
-            }else if(num==ele2){
-                count2++;
-            }
+        else if(count2==0){
+            ele2=num;
+            count2=1;
         }
-        vector<int>ans;
-        if(count1>nums.size()/3){
-            ans.push_back(ele1);
+        else{
+            count1--;
+            count2--;
         }
-        if(count2>nums.size()/3){
-            ans.push_back(ele2);
+      }
+      count1=0;
+      count2=0;
+      for(int i=0;i<n;i++){
+        if(nums[i]==ele1){
+            count1++;
+        } else if(nums[i]==ele2){
+            count2++;
         }
-        return ans;
+      }
+      vector<int>result;
+      if(count1>n/3){
+        result.push_back(ele1);
+      }
+      if(count2>n/3){
+        result.push_back(ele2);
+      }
+      return result;
     }
 };
