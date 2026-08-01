c252b367bbb56a77746afc1fff8ef5f4634742a0
Aman-coder14 <Amansingh81028@gmail.com>
2026-08-01 19:19:35 +0530

    Time: 9 ms (52.08%), Space: 11.2 MB (94.63%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,34 +2,53 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         unordered_map<char, int> mp;
+
+        // Step 1: Store frequency of every character in t
         for (char ch : t) {
             mp[ch]++;
         }
+        // Number of unique characters still needed
         int need = mp.size();
+        // Sliding window pointers
         int i = 0, j = 0;
+        // To store the answer
         int start = 0;
         int minLen = INT_MAX;
+
+        // Expand the window
         while (j < s.size()) {
+
+            // Include current character in the window
             mp[s[j]]--;
+
+            // If this character's requirement is completed
             if (mp[s[j]] == 0) {
                 need--;
             }
+            // If all required characters are present
             while (need == 0) {
+                // Update minimum window
                 if (j - i + 1 < minLen) {
-                    minLen=j-i+1;
-                    start=i;
+                    minLen = j - i + 1;
+                    start = i;
                 }
+                // Remove leftmost character
                 mp[s[i]]++;
-                if(mp[s[i]]>0){
+                // If we now need this character again,
+                // the window becomes invalid
+                if (mp[s[i]] > 0) {
                     need++;
                 }
+                // Shrink window
                 i++;
             }
+            // Expand window
             j++;
         }
-        if(minLen==INT_MAX){
+        // No valid window found
+        if (minLen == INT_MAX)
             return "";
-        }
+        // Return the smallest window
         return s.substr(start, minLen);
     }
 };
